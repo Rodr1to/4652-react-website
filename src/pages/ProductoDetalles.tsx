303a41163da1d206ee71b2789c79bf877e081eaf
rodr1to <rovalverdea@gmail.com>
2025-10-09 21:39:51 -0500

    chore: updated productodetalles table, productodetalles data, designed producto table and added table data and fixed items equals zero

--- src/pages/ProductoDetalles.tsx
+++ src/pages/ProductoDetalles.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import PageHeader from "../components/PageHeader"
 import { useEffect, useState } from "react"
 import type { Producto } from "../types/Producto"
+import "./ProductoDetalles.css"
 
 const ProductoDetalles = () => {
     const params = useParams()
@@ -30,12 +31,14 @@ const ProductoDetalles = () => {
 
             <section className="py-20">
                 <div className="max-w-7xl mx-auto px-3">
+                    <div className="flex -mx-3">
                      <div className="w-full md:w-1/3 px-3">
                         <img src={"https://servicios.campus.pe/" + (productoSeleccionado?.imagengrande ? productoSeleccionado.imagengrande : "imagenes/nofoto.jpg")}                     
                         alt="" className="w-full object-cover px-8 pt-8 transition-transform duration-500 hover:scale-115"/>
                     </div>
                     <div className="w-full md:w-2/3 px-3">
-                        <table className="table">
+                        <table className="tabla-detalle-producto mb-4">
+                            <tbody>
                             <tr>
                                 <th>Precio</th>
                                 <td> S/ {productoSeleccionado?.preciorebajado === 0 ? productoSeleccionado.precio.toFixed(2): productoSeleccionado?.preciorebajado.toFixed(2)}
@@ -45,23 +48,37 @@ const ProductoDetalles = () => {
                             </tr>
                             <tr>
                                 <th>Proveedor</th>
-                                <td></td>
+                                <td>{productoSeleccionado?.proveedor}</td>
                             </tr>
                             <tr>
                                 <th>Categoria</th>
-                                <td></td>
+                                <td>{productoSeleccionado?.categoria}</td>
+                            </tr>
+                                                        <tr>
+                                <th>Stock</th>
+                                <td>{productoSeleccionado?.unidadesenexistencia == 0 ? "No hay existencias.": productoSeleccionado?.unidadesenexistencia}</td>
                             </tr>
                             <tr>
                                 <th>Detalle</th>
-                                <td></td>
+                                <td>{productoSeleccionado?.detalle}</td>
                             </tr>
                             <tr>
                                 <th>Pais</th>
-                                <td></td>
+                                <td>{productoSeleccionado?.pais}</td>
                             </tr>
+                                <tr>
+                                <th>Atencion al cliente</th>
+                                <td>{productoSeleccionado?.telefono}</td>
+                            </tr>
+                            <tr>
+                                <th>Calificacion</th>
+                                <td>{productoSeleccionado?.promedioestrellas} ({productoSeleccionado?.totalcalificaciones} calificaciones)</td>
+                            </tr>
+                            </tbody>
                     </table>
                     <h3>Descripcion</h3>
                     <div dangerouslySetInnerHTML={{__html: productoSeleccionado?.descripcion || "" }}></div>
+                </div>
                 </div>
                 </div>
             </section>
